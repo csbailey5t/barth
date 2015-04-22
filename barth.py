3eb5ba95e03a1faa33ed1b82726d3b80e9a1fead
Scott Bailey <csb5t@virginia.edu>
2015-04-22 15:08:59 -0400

    Initial commit

---
+++ barth.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+TOC_URL = "http://solomon.dkbl.alexanderstreet.com/cgi-bin/asp/philo/dkbl/volumes_toc.pl?&church=ON"
+
+class Paragraphs:
+    def __init__(self, url, title, abstract, sections=None):
+        self.url = url
+        self.title = title
+        self.abstract = abstract
+        self.sections = sections or []
+
+class Section:
+    def __init__(self, number, title, contents=None):
+        self.number = number
+        self.title = title
+        self.contents = contents or []
+
+class SectionContents:
+    EXCURSUS = 0
+    TEXT = 1
+    PAGE_BREAK = 2
+
+    def __init__(self, content_type, data):
+        self.content_type = content_type
+        self.data = data
+
+
+def download_links(url, link_text):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text)
+
+    for a in soup.find_all('a'):
+        if link_text in a.get_text():
+            yield urljoin(page.url, a['href'])
+
+def download_volume(url):
+    for url in download_links(url, 'View Text'):
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text)
+        title = soup.find(class_='head').get_text()
+        abstract = '\n'.join(hibold.get_text() for hibold in soup.find_all(class_='hibold'))
+        p = Paragraph(url, title, abstract)
+
+def main(url = TOC_URL):
+    for url in download_links(url, 'Table of Contents'):
+        download_volume(url)
+
+if __name__ == '__main__':
+    main()
