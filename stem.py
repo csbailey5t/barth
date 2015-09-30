a70a17fd159026760230004479f02b7bdc06d86c
Scott Bailey <csb5t@virginia.edu>
2015-09-30 15:13:18 -0400

    Use nltk textcollection and tf_idf

--- stem.py
+++ stem.py
@@ -9,22 +9,27 @@ tokenized_corpus = logl.tokenize_corpus('paragraphs')
 stemmer = nltk.stem.snowball.SnowballStemmer('english')
 stopwords = set(nltk.corpus.stopwords.words('english'))
 
-cfdist = nltk.probability.ConditionalFreqDist()
+texts = []
 columns = []
 
 for (filename, words) in tokenized_corpus:
     filename = os.path.basename(filename)
     columns.append(filename)
-    for word in words:
-        if word in stopwords:
-            continue
-        cfdist[stemmer.stem(word)][filename] += 1
+    words = [
+        stemmer.stem(word)
+        for word in words
+        if word not in stopwords
+        ]
+    text = nltk.text.Text(words, filename)
+    texts.append(text)
+
+collection = nltk.text.TextCollection(texts)
 
 with open('freqencies.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow([None] + columns)
-    for word in cfdist:
+    for word in collection.vocab():
         row = [word]
         for column in columns:
-            row.append(cfdist[word][column])
+            row.append(collection.tf_idf(word, column))
         writer.writerow(row)
