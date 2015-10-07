32408b37214be3b5b6c2cccc2fe202f690d4e8e9
Scott Bailey <csb5t@virginia.edu>
2015-10-07 15:00:37 -0400

    Use a different library for tfidf

--- stem.py
+++ stem.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python3
 
 import nltk
-import logl
 import os
 import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-tokenized_corpus = logl.tokenize_corpus('paragraphs')
 stemmer = nltk.stem.snowball.SnowballStemmer('english')
-stopwords = set(nltk.corpus.stopwords.words('english'))
 
-texts = []
-columns = []
+path = 'paragraphs'
 
-for (filename, words) in tokenized_corpus:
-    filename = os.path.basename(filename)
-    columns.append(filename)
+
+def tokenizer(text):
+    tokens = nltk.word_tokenize(text)
     words = [
         stemmer.stem(word)
-        for word in words
-        if word not in stopwords
+        for word in tokens
+        if word.isalpha()
         ]
-    text = nltk.text.Text(words, filename)
-    texts.append(text)
+    return words
 
-collection = nltk.text.TextCollection(texts)
+all_files = []
+
+for dirpath, dirs, files in os.walk(path):
+    for f in files:
+        full_fn = os.path.join(dirpath, f)
+        all_files.append(full_fn)
+# all_files = all_files[:2]
+
+tfidf = TfidfVectorizer(
+    tokenizer=tokenizer, stop_words='english', input='filename'
+    )
+tfs = tfidf.fit_transform(all_files)
+print('minimum={}'.format(tfs.min()))
+print('maximum={}'.format(tfs.max()))
 
 with open('freqencies.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerow([None] + columns)
-    for word in collection.vocab():
-        row = [word]
-        for column in columns:
-            row.append(collection.tf_idf(word, column))
-        writer.writerow(row)
+    writer.writerow([None] + tfidf.get_feature_names())
+    for i, file in enumerate(all_files):
+        writer.writerow([os.path.basename(file)] + tfs.getrow(i).toarray().flatten().tolist())
