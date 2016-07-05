89031d9f42c0af37777eadaee63a139a669ac674
Eric Rochester <erochest@virginia.edu>
2016-07-05 15:35:53 -0400

    Fixed how election things are identified

--- barth/__init__.py
+++ barth/__init__.py
@@ -2,7 +2,7 @@
 
 def get_location(row):
     try:
-        return tuple([int(t) for t in row['loc'].split('.')][:2])
+        return int(row['loc'].split('.')[2])
     except:
         print('ERROR: {}'.format(type(row)))
         raise
@@ -10,14 +10,14 @@ def get_location(row):
 
 def is_before_election(row):
     loc = get_location(row)
-    return loc < (4, 1)
+    return loc < 33
 
 
 def is_election(row):
     loc = get_location(row)
-    return loc >= (4, 1) and loc <= (4, 5)
+    return loc == 33
 
 
 def is_after_election(row):
     loc = get_location(row)
-    return loc > (4, 5)
+    return loc > 33
